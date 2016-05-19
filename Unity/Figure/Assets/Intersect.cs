a0044ac480b1e49cc3123556e6c87ab21ad024c2
tkenshin <tsumurayakenshin@icloud.com>
2016-05-19 20:25:35 +0900

    誤字を修正 Intercept -> Intersect, コードを最適化

--- Unity/Figure/Assets/Intersect.cs
+++ Unity/Figure/Assets/Intersect.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class Intercept : MonoBehaviour {
+public class Intersect : MonoBehaviour {
 
     [SerializeField]
     private GameObject plane01;
@@ -21,7 +21,28 @@ public class Intercept : MonoBehaviour {
     private List<Vector3> plane01Vertices = new List<Vector3>();
     private List<Vector3> plane02Vertices = new List<Vector3>();
 
-	void Start () {
+    private Vector3 GetCrossProduct(Vector3 A, Vector3 B, Vector3 C)
+    {
+        // new Vector3(B.x - A.x, B.y - A.y, B.z - A.z);
+        // new Vector3(C.x - A.x, C.y - A.y, C.z - A.z);
+        var AB = B - A;
+        var AC = C - A;
+
+        // float a = (B.y - A.y) * (C.z - A.z) - (C.y - A.y) * (B.z - A.z);
+        // float b = (B.z - A.z) * (C.x - A.x) - (C.z - A.z) * (B.x - A.x);
+        // float c = (B.x - A.x) * (C.y - A.y) - (C.x - A.x) * (B.y - A.y);
+        // pd01 = new Vector3(a, b, c);
+        return Vector3.Cross(AB, AC);
+    }
+
+    private bool IsIntersectionLine()
+    {
+
+
+        return false;
+    }
+
+    void Start () {
         mf01 = plane01.GetComponent<MeshFilter>();
         mf02 = plane02.GetComponent<MeshFilter>();
 
@@ -74,26 +95,14 @@ public class Intercept : MonoBehaviour {
             var B = plane01Vertices[1];
             var C = plane01Vertices[2];
 
-			var AB = B - A; //new Vector3(B.x - A.x, B.y - A.y, B.z - A.z);
-			var AC = C - A; //new Vector3(C.x - A.x, C.y - A.y, C.z - A.z);
+            pd01 = GetCrossProduct(A, B, C);
 
-            float a = (B.y - A.y) * (C.z - A.z) - (C.y - A.y) * (B.z - A.z);
-            float b = (B.z - A.z) * (C.x - A.x) - (C.z - A.z) * (B.x - A.x);
-            float c = (B.x - A.x) * (C.y - A.y) - (C.x - A.x) * (B.y - A.y);
+            // d01 = -(a * A.x + b * A.y + c * A.z);
+            d01 = -(pd01.x * A.x + pd01.y * A.y + pd01.z * A.z);
 
-			var
-
-			d01 = -(a * A.x + b * A.y + c * A.z);
-
-			pd01 = new Vector3(a, b, c);
-
-            GameObject verSp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            var verSp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			verSp.transform.position = pd01;
             verSp.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
-
-            // ax + by + cz + d = 0;
-
-            Debug.Log(a + "x + " + b + "y + " + c + "z");
 
 			Debug.Log("n = " + pd01);
             Debug.Log("d = " + d01);
@@ -107,24 +116,13 @@ public class Intercept : MonoBehaviour {
             // PlaneVertex_02 = C
 
 			var A = plane02Vertices[0];
-            Vector3 B = plane02Vertices[1];
-            Vector3 C = plane02Vertices[2];
+            var B = plane02Vertices[1];
+            var C = plane02Vertices[2];
 
-            Vector3 AB = new Vector3(B.x - A.x, B.y - A.y, B.z - A.z);
-            Vector3 AC = new Vector3(C.x - A.x, C.y - A.y, C.z - A.z);
+            pd02 = GetCrossProduct(A, B, C);
 
-            float a = (B.y - A.y) * (C.z - A.z) - (C.y - A.y) * (B.z - A.z);
-            float b = (B.z - A.z) * (C.x - A.x) - (C.z - A.z) * (B.x - A.x);
-            float c = (B.x - A.x) * (C.y - A.y) - (C.x - A.x) * (B.y - A.y);
-
-			d02 = -(a * A.x + b * A.y + c * A.z);
-
-			var aa = B - A;
-			var cc = C - A;
-			var vv = Vector3.Cross(aa, cc);
-
-			//pd02 = new Vector3(a, b, c);
-			pd02 = vv;
+            // d02 = -(a * A.x + b * A.y + c * A.z);
+            d02 = -(pd02.x * A.x + pd02.y * A.y + pd02.z * A.z);
 
             GameObject verSp02 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			verSp02.transform.position = pd02;
@@ -139,14 +137,16 @@ public class Intercept : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Backspace))
 		{
 
-			// pd01 (a1, b1, c1)
-			// pd02 (a2, b2, c2)
-			// e = (b1 c2 - c1 b2, c1 a2 - a1 c2, a1 b2 - b1 a2)
-			float Ex = pd01.y * pd02.z - pd01.z * pd02.y;
-			float Ey = pd01.z * pd02.x - pd01.x * pd02.z;
-			float Ez = pd01.x * pd02.y - pd01.y * pd02.x;
+            // pd01 (a1, b1, c1)
+            // pd02 (a2, b2, c2)
+            // e = (b1 c2 - c1 b2, c1 a2 - a1 c2, a1 b2 - b1 a2)
 
-			Vector3 e = new Vector3(Ex, Ey, Ez);
+            //float Ex = pd01.y * pd02.z - pd01.z * pd02.y;
+            //float Ey = pd01.z * pd02.x - pd01.x * pd02.z;
+            //float Ez = pd01.x * pd02.y - pd01.y * pd02.x;
+
+            //Vector3 e = new Vector3(Ex, Ey, Ez);
+            var e = Vector3.Cross(pd01, pd02);
 
 			var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			obj.transform.position = e;
@@ -155,30 +155,32 @@ public class Intercept : MonoBehaviour {
 			Debug.Log("e = " + e);
 			if (0 != e.z)
 			{
-				pt.x = (d01 * pd02.y - d02 * pd01.y) / Ez;
-				pt.y = (d01 * pd02.x - d02 * pd01.x) / (-Ez);
+				pt.x = (d01 * pd02.y - d02 * pd01.y) / e.z;
+				pt.y = (d01 * pd02.x - d02 * pd01.x) / (-e.z);
 				pt.z = 0;
 
 			}
 			if (0 != e.y)
 			{
-				pt.x = (d01 * pd02.z - d02 * pd01.z) / (-Ey);
+				pt.x = (d01 * pd02.z - d02 * pd01.z) / (-e.y);
 				pt.y = 0;
-				pt.z = (d01 * pd02.x - d02 * pd01.x) / Ey;
+				pt.z = (d01 * pd02.x - d02 * pd01.x) / e.y;
 
 			}
 
 			if (0 != e.x)
 			{
 				pt.x = 0;
-				pt.y = (d01 * pd02.z - d02 * pd01.z) / Ex;
-				pt.z = (d01 * pd02.y - d02 * pd01.y) / (-Ex);
+				pt.y = (d01 * pd02.z - d02 * pd01.z) / e.x;
+				pt.z = (d01 * pd02.y - d02 * pd01.y) / (-e.x);
 
 			}
 
 			var obj02 = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 			obj02.transform.position = pt;
 			obj02.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+
+            Debug.Log("pt = " + pt);
 
 		}
 
